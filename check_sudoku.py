cc9f3d918c29acfc16d70d65d8516c762cea98ef
Srinivas Giduthuri <srinivas.giduthuri@glu.com>
2020-11-24 13:09:43 +0530

    Initial commit

---
+++ check_sudoku.py
@@ -0,0 +1,66 @@
+correct = [[1, 2, 3],
+           [2, 3, 1],
+           [3, 1, 2]]
+
+incorrect = [[1, 2, 3, 4],
+             [2, 3, 1, 3],
+             [3, 1, 2, 3],
+             [4, 4, 4, 4]]
+
+incorrect2 = [[1, 2, 3, 4],
+              [2, 3, 1, 4],
+              [4, 1, 2, 3],
+              [3, 4, 1, 2]]
+
+incorrect3 = [[1, 2, 3, 4, 5],
+              [2, 3, 1, 5, 6],
+              [4, 5, 2, 1, 3],
+              [3, 4, 5, 2, 1],
+              [5, 6, 4, 3, 2]]
+
+incorrect4 = [['a', 'b', 'c'],
+              ['b', 'c', 'a'],
+              ['c', 'a', 'b']]
+
+incorrect5 = [[1, 1.5],
+              [1.5, 1]]
+
+
+# Define a function check_sudoku() here:
+
+def check_sudoku(sudoku):
+    sudoku_len = len(sudoku[0])
+    for row in sudoku:
+        check_list = list(range(1, len(sudoku[0]) + 1))
+        for num in row:
+            print(num)
+            if num not in check_list:
+                return False
+            check_list.remove(num)
+    for index in range(sudoku_len):
+        check_list = list(range(1, len(sudoku[0]) + 1))
+        for row in sudoku:
+            print(row[index])
+            if row[index] not in check_list:
+                return False
+            check_list.remove(row[index])
+    return True
+
+
+print(check_sudoku(incorrect))
+# >>> False
+
+print(check_sudoku(correct))
+# >>> True
+
+print(check_sudoku(incorrect2))
+# >>> False
+
+print(check_sudoku(incorrect3))
+# >>> False
+
+print(check_sudoku(incorrect4))
+# >>> False
+
+print(check_sudoku(incorrect5))
+# >>> False
